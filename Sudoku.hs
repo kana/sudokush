2437935cf86e515cb04ed7b501320b0ad9ee1aa4
kana <nicht@s8.xrea.com>
2011-02-13 15:23:23 +0900

    Sudoku.pp: Implement

--- Sudoku.hs
+++ Sudoku.hs
@@ -85,7 +85,47 @@ parse :: String -> Puzzle
 parse = undefined
 
 pp :: Puzzle -> String
-pp = undefined
+pp (Puzzle g) = unlines $ concat $ (slice prows [1..3]
+                                    ++ border
+                                    ++ slice prows [4..6]
+                                    ++ border
+                                    ++ slice prows [7..9])
+  where
+    rows = [[g Map.! (r, c) | c <- [1..9]] | r <- [1..9]]
+    prows = map ppRow rows
+    border = [[replicate (length $ head $ head prows) '-']]
+    slice xs is = map (\i -> xs !! (i - 1)) is
+
+ppRow :: [Cell] -> [String]
+ppRow cs = join " | " $ [join " " $ slice pcs [1..3],
+                         join " " $ slice pcs [4..6],
+                         join " " $ slice pcs [7..9]]
+  where
+    pcs = map ppCell cs
+    slice xss is = map (\i -> xss !! (i - 1)) is
+    unzipN [] = []
+    unzipN xss
+      | any null xss = []
+      | otherwise = heads : unzipN tails
+        where
+          heads = map head xss
+          tails = map tail xss
+    join sep xss = go $ unzipN xss
+      where
+        go [] = []
+        go (xs:xss') = join' sep xs : go xss'
+    join' sep xs = foldr1 (\x1 x2 -> x1 ++ sep ++ x2) xs
+
+ppCell :: Cell -> [String]
+ppCell Cell {solution = s, candidates = cs}
+  | s /= 0 = ["###", "#" ++ show s ++ "#", "###"]
+  | otherwise = [slice pcs [1..3], slice pcs [4..6], slice pcs [7..9]]
+    where
+      ppCandidates c
+        | c `elem` cs = show c
+        | otherwise = "."
+      pcs = map ppCandidates [1..9]
+      slice xs is = concat $ map (\i -> xs !! (i - 1)) is
 
 
 
